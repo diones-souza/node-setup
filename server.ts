110e261b3946f11845bc56b3e51d7a326c6c00e0
diones-souza <51972715+diones-souza@users.noreply.github.com>
2022-07-22 16:47:16 -0400

    Update server.ts

--- server.ts
+++ server.ts
@@ -4,7 +4,6 @@ async function main() {
     const modules = new Routes()
     const app = await modules.handle()
     const port = process.env.APP_PORT
-    console.log(process.env.DATABASE_URL)
 
     app.listen(port, () => console.log(`Server is runnig on Port ${port}`))
 }
